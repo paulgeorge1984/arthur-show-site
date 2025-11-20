a14da0693bebb521bdc08240a24db6b127fec515
paulgeorge1984 <jupiterjoji@ondorinohane.com>
2025-11-21 04:40:54 +0900

    test

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,8 @@
+
+export default new Map([
+["src/content/daily-messages/2025-11-17.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-17.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-18.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-18.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-21.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-21.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-20.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-20.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-19.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-19.mdx&astroContentModuleFlag=true")]]);
+		
