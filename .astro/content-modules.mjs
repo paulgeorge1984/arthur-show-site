cfa9178aaa294fe53eaf13135d4d3102c42c2a11
paulgeorge1984 <jupiterjoji@ondorinohane.com>
2025-11-24 00:04:37 +0900

    1124#

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,12 @@
 
 export default new Map([
-["src/content/daily-messages/2025-11-18.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-18.mdx&astroContentModuleFlag=true")],
 ["src/content/daily-messages/2025-11-17.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-17.mdx&astroContentModuleFlag=true")],
-["src/content/daily-messages/2025-11-19.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-19.mdx&astroContentModuleFlag=true")],
 ["src/content/daily-messages/2025-11-21.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-21.mdx&astroContentModuleFlag=true")],
 ["src/content/daily-messages/2025-11-20.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-20.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-18.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-18.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-19.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-19.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-22.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-22.mdx&astroContentModuleFlag=true")],
 ["src/content/daily-messages/2025-11-23.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-23.mdx&astroContentModuleFlag=true")],
-["src/content/daily-messages/2025-11-22.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-22.mdx&astroContentModuleFlag=true")]]);
+["src/content/daily-messages/ArthurHollands_Okinawa_Kendo_Speech.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2FArthurHollands_Okinawa_Kendo_Speech.mdx&astroContentModuleFlag=true")],
+["src/content/daily-messages/2025-11-24.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdaily-messages%2F2025-11-24.mdx&astroContentModuleFlag=true")]]);
 		
